6cb0835b4f33a5f0ecc419ce2a3b06a2c9d49fb9
Jordi Albert <63541019+jarg1023@users.noreply.github.com>
2022-08-17 13:00:36 +0200

    #4 | Implement code to make tests go green

--- src/challenge-3/challenge-3.js
+++ src/challenge-3/challenge-3.js
@@ -1,10 +1,16 @@
 /*
- * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Challenge #3
+ * IS IT A PRIME NUMBER?
+ * Level: MEDIUM
+ *
+ * Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
  * Hecho esto, imprime los números primos entre 1 y 100.
  */
 
+main()
+
 function main() {
-    for (let index = 0; index <= 100; index++) {
+    for (let index = 1; index <= 100; index++) {
         if (isPrimeNumber(index)) {
             console.log(index);
         }
@@ -12,7 +18,19 @@ function main() {
 }
 
 function isPrimeNumber(number) {
-    console.log(number);
+    if (number < 2) {
+        return false
+    }
+
+    squareRoot = Math.floor(Math.sqrt(number));
+
+    for (let index = squareRoot; index >= 2; index--) {
+        if ((number % index) === 0) {
+            return false
+        }
+    }
+
+    return true
 }
 
 module.exports = isPrimeNumber
