f46a8fec9f23792c3f859125e736fc68c0184eff
Jordi Albert <63541019+jarg1023@users.noreply.github.com>
2022-08-18 16:32:22 +0200

    #5 | Create challenge 4 files

---
+++ src/challenge-4/challenge-4.js
@@ -0,0 +1,16 @@
+/*
+ * Challenge #4
+ * AREA OF A POLYGON
+ * Level: EASY
+ *
+ * Enunciado: Crea UNA ÚNICA FUNCIÓN (importante que sólo sea una) que sea capaz de calcular y retornar el área de un polígono.
+ * - La función recibirá por parámetro sólo UN polígono a la vez.
+ * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+ * - Imprime el cálculo del área de un polígono de cada tipo.
+ */
+
+function calculatePolygonArea() {
+    return "a"
+}
+
+module.exports = calculatePolygonArea
